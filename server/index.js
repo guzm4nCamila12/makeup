f5a6de08367022f5a8c7b2e2b22ca93a023f0ecf
guzm4nCamila12 <mariaguramos12@gmail.com>
2024-05-28 15:26:25 -0500

    Axios

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios')
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json());
