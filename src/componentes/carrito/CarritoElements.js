e3372544ef7c446743d9c7d617b6314d2b0af17f
guzm4nCamila12 <mariaguramos12@gmail.com>
2024-06-24 11:35:13 -0500

    Terminando carrito

--- src/componentes/carrito/CarritoElements.js
+++ src/componentes/carrito/CarritoElements.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { dataContext } from "../context/DataContext";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function CarritoElements() {
     const { makeupCarrito, setMakeupCarrito } = useContext(dataContext)
@@ -11,7 +12,7 @@ function CarritoElements() {
 
     {/*const cantidad = () => {
         setMakeupCarrito((actualesProductos) => {
-            return actualesProductos.map((libs) => {
+            return actualesProductos.map((producto) => {
                 const valor = document.getElementById(libs.id)?.value || libs.cantidad;
                 return { ...libs, precioCarrito: libs.precio * valor.value, cantidad: valor.value }
             })
@@ -32,186 +33,57 @@ function CarritoElements() {
     }
 
     return makeupCarrito.map((producto) => {
+        if (!producto || !producto.image) {
+            return null; // o maneja el caso donde image es undefined
+        }
 
         return (
-            <section class="h-100">
-                <div class="container h-100 py-5">
-                    <div class="row d-flex justify-content-center align-items-center h-100">
-                        <div class="col-10">
-
-                            <div class="d-flex justify-content-between align-items-center mb-4">
-                                <h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>
-                                <div>
-                                    <p class="mb-0"><span class="text-muted">Sort by:</span> <a href="#!" class="text-body">price <i
-                                        class="fas fa-angle-down mt-1"></i></a></p>
-                                </div>
-                            </div>
-
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
+            <section className="h-100" key={producto.id}>
+                <div className="container h-100 py-2">
+                    <div className="row d-flex justify-content-center align-items-center h-100">
+                        <div className="col-10">
+                            <div className="card rounded-3 mb-4">
+                                <div className="card-body p-4">
+                                    <div className="row d-flex justify-content-between align-items-center">
+                                        <div className="col-md-2 col-lg-2 col-xl-2">
                                             <img
-                                                src={producto.image} className="img-fluid rounded -3" alt="imagen"></img>
+                                                src={producto.image}
+                                                className="img-fluid rounded-3"
+                                                alt={producto.tittle}
+                                            />
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p className="lead fw-normal mb-2">{producto.title}</p>
-                                            <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
+                                        <div className="col-md-3 col-lg-3 col-xl-3">
+                                            <p className="lead fw-normal mb-2">{producto.tittle}</p>
+                                            <p><span className="text-muted"></span>{producto.description}</p>
                                         </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            {/*<button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
-                                            </button>*/}
-
-                                            <input id={producto.id} min="1" name="quantity" type="number"
-                                                class="form-control form-control-sm" value={producto.cantidad} onChange={cantidad} />
-
-                                            {/*<button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
-                                            </button>*/}
+                                        <div className="col-md-3 col-lg-3 col-xl-2 d-flex">
+                                            <input
+                                                id={producto.id}
+                                                min="1"
+                                                name="quantity"
+                                                value={producto.cantidad}
+                                                type="number"
+                                                className="form-control form-control-sm"
+                                                onChange={cantidad}
+                                            />
                                         </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">${formatoNumero(producto.precioCarrito)}</h5>
+                                        <div className="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                                            <h5 className="mb-0">$ {formatoNumero(producto.precioCarrito)}</h5>
                                         </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <button className="btn btn-danger" id={producto.id} onClick={eliminarProducto}><i class="bi bi-trash3-fill"></i></button>
+                                        <div className="col-md-1 col-lg-1 col-xl-1 text-end">
+                                            <button className="btn btn-dark" id={producto.id} onClick={eliminarProducto}>
+                                                <DeleteIcon />
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
-                                            <img
-                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Basic T-shirt</p>
-                                            <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
-                                            </button>
-
-                                            <input id="form1" min="0" name="quantity" value="2" type="number"
-                                                class="form-control form-control-sm" />
-
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
-                                            </button>
-                                        </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
-                                        </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
-                                            <img
-                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Basic T-shirt</p>
-                                            <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
-                                            </button>
-
-                                            <input id="form1" min="0" name="quantity" value="2" type="number"
-                                                class="form-control form-control-sm" />
-
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
-                                            </button>
-                                        </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
-                                        </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card rounded-3 mb-4">
-                                <div class="card-body p-4">
-                                    <div class="row d-flex justify-content-between align-items-center">
-                                        <div class="col-md-2 col-lg-2 col-xl-2">
-                                            <img
-                                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                                                class="img-fluid rounded-3" alt="Cotton T-shirt" />
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-3">
-                                            <p class="lead fw-normal mb-2">Basic T-shirt</p>
-                                            <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
-                                        </div>
-                                        <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                                <i class="fas fa-minus"></i>
-                                            </button>
-
-                                            <input id="form1" min="0" name="quantity" value="2" type="number"
-                                                class="form-control form-control-sm" />
-
-                                            <button data-mdb-button-init data-mdb-ripple-init class="btn btn-link px-2"
-                                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                                <i class="fas fa-plus"></i>
-                                            </button>
-                                        </div>
-                                        <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                            <h5 class="mb-0">$499.00</h5>
-                                        </div>
-                                        <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                            <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="card mb-4">
-                                <div class="card-body p-4 d-flex flex-row">
-                                    <div data-mdb-input-init class="form-outline flex-fill">
-                                        <input type="text" id="form1" class="form-control form-control-lg" />
-                                        <label class="form-label" for="form1">Discound code</label>
-                                    </div>
-                                    <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-outline-warning btn-lg ms-3">Apply</button>
-                                </div>
-                            </div>
-
-                            <div class="card">
-                                <div class="card-body">
-                                    <button type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>
-                                </div>
-                            </div>
-
                         </div>
                     </div>
                 </div>
             </section>
-        )
-    })
+        );
+    });
 }
 
 export default CarritoElements;
