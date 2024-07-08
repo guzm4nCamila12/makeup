204c2b33a10a379ea292417ab4497052ce9d75e1
guzm4nCamila12 <mariaguramos12@gmail.com>
2024-07-08 16:42:49 -0500

    Cambiando

--- server/configDB/configDB.js
+++ server/configDB/configDB.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "",
-    database: "bdEcommerceLocal",
+    database: "ecommerce",
     port: 3306,
 })
 
